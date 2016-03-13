92f2c5f004449ce2e41ed7e8a4016a1cab436f81
Sergey <Sergey>
2016-03-13 12:12:05 +0200

    cleared trash from visibility.js

--- ext/visibility.js
+++ ext/visibility.js
@@ -1,8 +1,7 @@
 /*
  * Author: Sergey Starodub 
- * Author URI:
- *
- * Description: Checks if a DOM element is truly visible.*/
+ * Description: Checks if a DOM element is truly visible.
+ */
 
 const VISIBLE_PADDING_TOP = 0.01; // 1% of element's width or/and height may be not visible.
 const VISIBLE_PADDING_BOTTOM = 0.7;
@@ -76,37 +75,3 @@ function visibleInPoint(x, y, obj, wndw){
 }
 
 export {isVisible};
-
-
-
-// function visibleInPoint(x, y, obj, wndw){
-//     wndw = wndw || window;
-//     let elmsFromPoint = Array.from(wndw.document.elementsFromPoint(x, y));
-//     let i = 0;
-//     let elmsLength = elmsFromPoint.length;
-//     while (elmsFromPoint[i] !== obj && i<=elmsLength){
-//         i++;
-//     }
-//     if (elmsFromPoint[i] === obj){
-//         if (i === 0){
-//             return true;
-//         }
-//         for (let j; j <= i; j++)
-//         {
-//             if (!transparent(elmsFromPoint[j])){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-//     return false;
-// }
-
-// function transparent(obj){
-//     for (let stl of tStyles){
-//         if (obj.tagNamef != "IMG" && _getStyle(obj, stl[0]) === stl[1]){
-//             return true;
-//         }
-//     }
-//     return false;
-// }

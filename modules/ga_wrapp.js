8b01d3e136cfd063a5acc4c1de02443053f26031
Sergey <Sergey>
2016-03-22 17:18:05 +0200

    added ga_wrapper

---
+++ modules/ga_wrapp.js
@@ -0,0 +1,41 @@
+import runGASnippet from 'ext/ga-snippet';
+import {GA_CODE} from 'constants';
+
+let ga = {
+	loaded: () => {
+		new Promise((resolve, reject) => {
+                logList.push("start _isLoaded function");
+                runGASnippet();
+                ga(resolve);
+        });
+	},
+    init: (cliendId) => {
+    	new Promise((resolve,reject) => {
+                logList.push("start init function");
+                if (clientId){
+                	ga('create', GA_CODE, {
+                    	name: this.identifier,
+                    	clientId: clientId
+                	});
+                else {
+                	ga('create', GA_CODE, {
+                    name: this.identifier,
+                	});
+                }
+                ga(resolve);
+        });
+    },
+ 	track: (eventName, properties) => {
+            this.logList.push("start track function");
+            let evCat = 'ALL';
+            let evAct = eventName;
+            let evLabel = '';
+            ga(this.identifier+'.send', {
+                hitType: 'event',
+                eventCategory: evCat,
+                eventAction: evAct,
+                eventLabel: evLabel
+        });
+    }   	
+};
+export{ga};

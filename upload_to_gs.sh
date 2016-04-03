7034e443e05380ca804bd867e7c18db651a6b3bc
Lior Sabag <lior@trendiguru.com>
2016-04-03 07:34:12 +0300

    Simple script to bundle and upload google storage. TODO: Automatically change constants.js (HOST_DOMAIN) and app.html (React dev->prod)

--- upload_to_gs.sh
+++ upload_to_gs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-gsutil cp -a public-read fzz.min.js gs://fzz
-gsutil cp -a public-read app/b_desktop.js gs://fzz/app/b_desktop.js
-gsutil cp -a public-read app/b_mobile.js gs://fzz/app/b_mobile.js
-gsutil cp -a public-read plugin/css/plugin.css gs://fzz/plugin/css/plugin.css
-gsutil cp -a public-read app/desktop.html  gs://fzz/app/desktop.html
-gsutil cp -a public-read app/css/app.css   gs://fzz/app/css/app.css 
+npm run deploy
+gsutil -m cp -a public-read fzz.min.js gs://fzz
+gsutil -m cp -a public-read b_plugin.js gs://fzz
+gsutil -m cp -a public-read app/b_desktop.js gs://fzz/app/b_desktop.js
+gsutil -m cp -a public-read app/b_mobile.js gs://fzz/app/b_mobile.js
+gsutil -m cp -a public-read plugin/css/plugin.css gs://fzz/plugin/css/plugin.css
+gsutil -m cp -a public-read app/desktop.html  gs://fzz/app/desktop.html
+gsutil -m cp -a public-read app/css/app.css   gs://fzz/app/css/app.css 
 gsutil -m mv -p gs://fzz/_old/img gs://fzz/
 gsutil -m setmeta -r -h "Cache-Control:public, max-age=5" gs://fzz

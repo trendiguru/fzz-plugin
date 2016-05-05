631b3328519608d33d29c41709efcb1763165208
Lior Sabag <lior@trendiguru.com>
2016-05-05 17:13:13 +0300

    Re-commit changes.

--- app/js/desktop.js
+++ app/js/desktop.js
@@ -8,37 +8,40 @@ import {analytics} from 'modules/analytics_wrapper';
 import App from './app';
 //developer tools
 import {REQUESTS} from 'modules/devTools';
+//utils
+import {getLocation} from 'modules/utils';
 
-let publisherDomain;
+let publisherDomain = getLocation(document.referrer).hostname.replace('www.', '');
 let imageURL;
-REQUESTS.desktop = 0;
-analytics.initializeInApp({refererDomain: window.location.hostname.replace("www.", ""), publisherDomain: publisherDomain});
 
+REQUESTS.desktop = 0;
+
+analytics.initializeInApp({publisherDomain: publisherDomain}); 
 
 /*------ RENDER ------*/
 
 function close () {
     window.parent.postMessage('hide', '*');
-    window.app = ReactDOM.render(React.createElement(App, {onMount: attachAnalytics, close: close}), document.getElementById('main'));
+    window.app = ReactDOM.render(React.createElement(App, {onMount:()=>{analytics.track('App Window Closed');}, close: close}), document.getElementById('main'));
 }
 
-window.app = ReactDOM.render(React.createElement(App, {onMount: attachAnalytics, close: close}), document.getElementById('main'));
+window.app = ReactDOM.render(React.createElement(App, {onMount:()=>{analytics.track('App Loaded');}, close: close}), document.getElementById('main'));
 
 document.getElementById('shadow').addEventListener('click', close);
 
 /*------ RECIEVE MESSAGES FROM MAIN ------*/
 
 window.addEventListener('message', msg => {
-    console.log('FZZ: iframe received message: ' + msg);
+    //console.log('FZZ: iframe received message: ' + msg);
     if (window.app.props.imageURL !== msg.data.imageURL) {
-        publisherDomain = msg.data.publisherDomain;
+        //publisherDomain = getLocation(msg.origin).hostname.replace('www.', '');
         imageURL = msg.data.imageURL;
         getImageData(msg.data.imageURL).then(data => {
             window.app = ReactDOM.render(
                 React.createElement(App, {onMount: attachAnalytics, close: close, imageURL: msg.data.imageURL, items: data.items}),
                 document.getElementById('main')
             );
-            console.log(window.app.props);
+            //console.log(window.app.props);
         });
     }
 });

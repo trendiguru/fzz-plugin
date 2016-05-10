158631cfe0da3bde649fefb09257411a0c18c969
Sergey Starodub <starodub1995@ukr.net>
2016-05-10 15:14:21 +0300

    works

--- modules/nginx_analytics.js
+++ modules/nginx_analytics.js
@@ -1,4 +1,5 @@
 import {entries} from 'modules/utils';
+import timeme from 'ext/timeme';
 
 let nginx = {
     // more fields are added in init()
@@ -62,5 +63,14 @@ function viewport() {
     }
     return viewport;
 }
+
+
+timeme();
+window.onbeforeunload = function (event) {
+    xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","https://track.trendi.guru/tr/web?event=Page%20Unloaded&duration=" + TimeMe.getTimeOnCurrentPageInSeconds(),false);
+    xmlhttp.send();
+
+};
 
 export default nginx;

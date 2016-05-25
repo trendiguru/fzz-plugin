f0c96748000917690b1d4a70a7bb56ac605d8cbe
Iddan Aharonson <mail@iddan.co>
2016-05-26 01:22:04 +0300

    Working draft

--- plugin/js/plugin.js
+++ plugin/js/plugin.js
@@ -1,21 +1,16 @@
-/* eslint-disable no-console, no-unused-vars */
+/* eslint-disable no-console */
 
 import domready from 'ext/domready';
 import * as constants from 'constants';
 import {analytics} from 'modules/analytics_wrapper';
 import draw from './draw';
 import {scanForever, observe} from './observe';
-import imagesLoaded from 'imagesloaded';
-import {smartCheckRelevancy} from 'modules/server';
-import {getElementsToProcess} from 'modules/utils';
+import Controller from './controller';
+//import {getElementsToProcess} from 'modules/utils';
 //import {console} from 'modules/smartConsole';
 
-const {USER_CONFIG, MIN_IMG_WIDTH, MIN_IMG_HEIGHT, IFRAME_ID, CSS_URL, IFRAME_SRC, PID} = constants;
+const {IFRAME_ID, CSS_URL, IFRAME_SRC, PID} = constants;
 
-const FZZ = window.FZZ = window.FZZ || {};
-let relevantImgs = FZZ.relevantImgs = {};
-let irrelevantImgs = FZZ.irrelevantImgs = {};
-let irrelevantElements = FZZ.irrelevantElements = {};
 let refererDomain = window.location.hostname.replace('www.', '');
 analytics.initializeInPublisher( {refererDomain: refererDomain, publisherDomain: refererDomain, PID:PID });
 analytics.track('Page Hit');
@@ -29,129 +24,15 @@ window.addEventListener('scroll', function () {
     }
 });
 
+let controller = window.controller = new Controller (draw);
 
 domready(function () {
     loadStyle();
     console.log('FZZ: domready');
     document.body.appendChild(createIframe());
-    scanForever(document.body, processElement);
-    observe(document.body, processElement, {childList: true,subtree: true});
+    scanForever(document.body, controller.process.bind(controller));
+    observe(document.body, controller.process.bind(controller), {childList: true,subtree: true});
 });
-
-function processElement(el) {
-    return Promise.resolve(el)
-        .then(function (elem) {
-            return new TGImage(elem);
-        })
-        .then(ensureNew)
-        .then(isLoaded)
-        .then(ensureSuspicious)
-        .then(smartCheckRelevancy)
-        .then(
-        function (relevantImg) {
-            let date = new Date();
-            console.log(`${date}: Found Relevant!: ${relevantImg.url}`);
-            relevantImgs[relevantImg.url] = relevantImg;
-            draw(relevantImg);
-        },
-        function (irrelevantImg) {
-            // This will only have a url if it returns from smartRelevacyCheck as irrelevant,
-            // the others will arrive as {name: nnn, element:eee} error objects.
-            if (irrelevantImg.url) {
-                irrelevantImgs[irrelevantImg.url] = irrelevantImg;
-            } else {
-                logIrrelevant(irrelevantImg);
-            }
-        });
-}
-
-function TGImage(elem, url) {
-    if (elem.nodeName == 'IMG') {
-        this.url = url || elem.src || (location.origin + elem.srcset);
-    } else {
-        //TODO: Get the size if possible
-        this.url = url || getBackgroundImage(elem);
-        if (!this.url) {
-            throw {
-                name: 'No Image found',
-                element: elem
-            };
-        }
-    }
-    this.element = elem;
-}
-
-function logIrrelevant(error) {
-    //console.log('reached logIrrelevant');
-    let errName = error.name;
-    let errElement = error.element;
-    let errorCounts = irrelevantElements[errName] = irrelevantElements[errName] || {};
-    let errorCountforElem = errorCounts[errElement] = errorCounts[errElement] || 0;
-    errorCountforElem += 1;
-}
-
-function ensureNew(tgImg) {
-    if (tgImg.url in relevantImgs || tgImg.url in irrelevantImgs) {
-        throw {
-            name: 'Not a New Image',
-            element: tgImg
-        };
-    }
-    return tgImg;
-}
-
-function isLoaded(tgImg) {
-    return new Promise(function (resolve, reject) {
-        let iml = imagesLoaded(tgImg.element, {
-            background: tgImg.background
-        });
-        iml.on('done', function (instance) {
-            resolve(tgImg);
-        });
-        iml.on('fail', function (instance) {
-            reject({
-                name: 'Image Load Failed',
-                element: tgImg
-            });
-        });
-    });
-}
-
-function ensureSuspicious(tgImg) {
-    let rect = tgImg.element.getBoundingClientRect();
-    if (rect.height >= MIN_IMG_HEIGHT && rect.width >= MIN_IMG_WIDTH) {
-        return tgImg;
-    } else {
-        throw {
-            name: 'Too Small',
-            element: tgImg
-        };
-    }
-}
-
-//From imagesLoaded
-function getBackgroundImage(elem) {
-    var urls = [];
-    // IE8
-    var getStyle = window.getComputedStyle || function (elem) {
-        return elem.currentStyle;
-    };
-    var style = getStyle(elem);
-    // get url inside url('...')
-    var reURL = /url\(([''""])?(.*?)\1\)/gi;
-    var matches = reURL.exec(style.backgroundImage);
-    while (matches !== null) {
-        var url = matches && matches[2];
-        if (url) {
-            urls.push(url);
-        }
-        matches = reURL.exec(style.backgroundImage);
-    }
-    if (urls.length > 1) {
-        console.log('Too many background images');
-    }
-    return urls[0];
-}
 
 function createIframe(src) {
     let iframe = document.createElement('iframe');

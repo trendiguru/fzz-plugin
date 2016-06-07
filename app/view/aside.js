5f8da3dbdefd03c43035cf5498ee0a47711afd9e
Iddan Aharonson <mail@iddan.co>
2016-06-08 01:32:11 +0300

    width into maxWidth

--- app/view/aside.js
+++ app/view/aside.js
@@ -5,9 +5,23 @@ const {Component} = React;
 class Aside extends Component {
     constructor (props) {
         super(props);
+        this.state = {
+            width: 0,
+            height: 0
+        };
+    }
+    componentDidMount () {
+        let img = new Image();
+        img.src = this.props.imageURL;
+        img.onload = () => {
+            let {width, height} = img;
+            this.setState({width, height});
+            this.forceUpdate();
+        };
     }
     render () {
-        return <aside style={{backgroundImage: `url('${this.props.imageURL}')`}}></aside>;
+        let {height, width} = this.state;
+        return <aside style={{width, height, backgroundImage: `url('${this.props.imageURL}')`}}></aside>;
     }
 }
 

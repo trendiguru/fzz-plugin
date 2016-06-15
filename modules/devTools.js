7392a7dca2f3a7c036fb2a9ab9618bd6280aa5db
Iddan Aharonson <mail@iddan.co>
2016-06-16 00:47:21 +0300

    Cleaned devtools

--- modules/devTools.js
+++ modules/devTools.js
@@ -1,130 +1,99 @@
-import {getDomainName, entries} from 'modules/utils';
-const devTools = window.devTools || {};
+import {getDomainName} from 'modules/utils';
 
-
-let active = true; //TODO: get "ective" variable from the current environment variable.
-let OPACITY = "0.01";
-window.devTools = devTools;
-let MUT = devTools.MUT = devTools.MUT || {
-	active: active,
-	srcMut: [],
-	nodeMut: [],
-	attrMut: [],
-	mainObserver: [],
-	observers: [],
-	set: (obj, mType)=>{
-		let owner = devTools.MUT;
-		if (owner.active === true){
-			if (mType === "node"){
-				owner.nodeMut.push(obj);
-			}
-			if (mType === "attribute"){
-				owner.attrMut.push(obj);
-			}
-			if (mType === "src"){
-				owner.srcMut.push(obj);
-			}
-			if (mType === "mainObserver"){
-				owner.mainObserver.push(obj);
-			}
-			if (mType === "observer"){
-				owner.observers.push(obj);
-			}
-		}
-	}
+let active = true; //TODO: get 'ective' variable from the current environment variable.
+// let OPACITY = '0.01';
+export let modules = {};
+export let MUT = {
+    active,
+    srcMut: [],
+    nodeMut: [],
+    attrMut: [],
+    mainObserver: [],
+    observers: [],
+    _node (obj) {this.nodeMut.push(obj);},
+    _attribute (obj) {this.attrMut.push(obj);},
+    _src (obj) {this.srcMut.push(obj);},
+    _mainObserver (obj) {this.mainObserver.push(obj);},
+    _observer (obj) {this.observers.push(obj);},
+    set (obj, mType) {
+        mType = `_${mType}`;
+        if (this.active === true && this[mType]) {
+            this[mType](obj);
+        }
+    }
 };
 
-let REQUESTS = devTools.REQUESTS = devTools.REQUESTS || {
-	active: active,
-	queue: [],
-	set: (reuestProperties, mType) => {
-		if (REQUESTS.active){
-			if (mType === "property"){
-				REQUESTS.queue.push(reuestProperties);
-			}
-		}
-	}
+export let REQUESTS = {
+    active,
+    queue: [],
+    set (reuestProperties, mType) {
+        if (REQUESTS.active && mType === 'property') {
+            REQUESTS.queue.push(reuestProperties);
+        }
+    }
 };
 
-let STACKS = devTools.STACKS = devTools.STACKS || {
-	active: active,
-	storage: {},
-	sColor:"RED",
-	newStack: (name)=>{
-		if (STACKS.active){
-			STACKS.storage[name] = [];
-		}
-	},
-	set: (sName, elem)=>{
-		if (STACKS.active){
-			STACKS.storage[sName].push(elem);
-		}
-	},
-	show: (sName, col)=>{
-		if (STACKS.active){
-			for (let [key, stack] of entries(STACKS.storage)) {
-				if (sName === key){
-					for (let elem of stack){
-						if (elem!==undefined && elem.style !== undefined){
-							elem.style.border = "12px solid";
-							elem.style.borderColor = STACKS.sColor;
-						}
-					}
-				}
-			}
-		}
-	},
-	hide:(sName)=>{
-		if (STACKS.active){
-			for (let [key, stack] of entries(STACKS.storage)) {
-				if (sName === key){
-					for (let elem of stack){
-						if (elem!==undefined && elem.style !== undefined){
-							elem.style.border = "0px solid";
-						}
-					}
-				}
-			}
-		}
-	}
+export let STACKS = {
+    active: active,
+    storage: {},
+    sColor: 'RED',
+    newStack (name) {
+        if (STACKS.active) {
+            STACKS.storage[name] = [];
+        }
+    },
+    set (sName, elem) {
+        if (STACKS.active) {
+            STACKS.storage[sName].push(elem);
+        }
+    },
+    show (sName) { // +col
+        return this.modify(sName, 12);
+    },
+    hide (sName) {
+        return this.modify(sName, 0);
+    },
+    modify (sName, borderWidth = 0) {
+        if (STACKS.active) {
+            for (let key in STACKS.storage) {
+                let stack = STACKS.storage[key];
+                if (sName === key) {
+                    for (let elem of stack) {
+                        if (elem !== undefined && elem.style !== undefined) {
+                            elem.style.border = `${borderWidth}px solid ${STACKS.sColor}`;
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
 
-let s = STACKS;
-if (active){
-//______STACKS_definition_____//
-	s.newStack("isNew");
-	s.newStack("isLoaded");
-	s.newStack("isSuspicious");
-	s.newStack("TGImage");
-	s.newStack("smartCheckRelevancy");
-	s.newStack("process");
-	s.newStack("relevantImg");
-	s.newStack("irrelevantImg");
-	s.newStack("logIrrelevant");
-	s.newStack("smartCheckRelevancy_input");
-	s.newStack("svg");
-	s.newStack("content");
+if (active) {
+    //______STACKS_definition_____//
+    ['isNew', 'isLoaded', 'isSuspicious', 'TGImage', 'smartCheckRelevancy', 'process', 'relevantImg', 'irrelevantImg', 'logIrrelevant', 'smartCheckRelevancy_input', 'svg', 'content'].forEach(stack => STACKS.newStack(stack));
 
-	//______ective_functions______//
-	devTools.modules = {};
-	let utils = devTools.modules.utils = {};
-	utils.getDomainName = getDomainName;
+    //______ective_functions______//
+    modules = {};
+    modules.utils = {getDomainName};
 }
 
-devTools.coloredReport = ()=>{
-	let defaultColor  = s.sColor;
-	s.sColor = "yellow";
-	s.show("smartCheckRelevancy_input");
-	s.sColor = "green";
-	s.show("relevantImg");
-	s.sColor = "red"
-	s.show("irrelevantImg");
-	s.sColor = defaultColor;
-};
-devTools.clrscrn = ()=>{
-	for (let key in s.storage){
-		s.hide(key);
-	}
-};
+export function coloredReport () {
+    let {sColor, show} = STACKS,
+        defaultColor = sColor;
+    sColor = 'yellow';
+    show('smartCheckRelevancy_input');
+    sColor = 'green';
+    show('relevantImg');
+    sColor = 'red';
+    show('irrelevantImg');
+    STACKS.sColor = defaultColor;
+}
 
-export {MUT, REQUESTS, STACKS, devTools};
+export function clrscrn () {
+    for (let key in STACKS.storage) {
+        STACKS.hide(key);
+    }
+}
+
+window.devTools = window.devTools || {MUT, REQUESTS, STACKS, coloredReport, clrscrn, modules};

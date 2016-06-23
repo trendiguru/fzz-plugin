7c0f58b5d520856b093b95aa42e1c4d7e77123b1
Lior Sabag <lior@trendiguru.com>
2016-06-23 20:00:50 +0300

    * Added es6_run_ext.js to webpack config. * Change data-pid to `dev`.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
         'app/b_app.js': ['whatwg-fetch', 'babel-polyfill', './app/main.js'],
         'app/css/b_app.css': './app/css/app.css',
         'demo/b_demo.js': ['whatwg-fetch', 'babel-polyfill', './demo/main.js'],
-        'demo/b_demo.css': './demo/main.css'
+        'demo/b_demo.css': './demo/main.css',
+        'extensions/chrome/run_ext.js': 'extensions/chrome/es6_run_ext.js'
     },
     output: {
         path: '.',

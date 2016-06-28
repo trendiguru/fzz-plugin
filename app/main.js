e69c2480e134e6a652d376c60678ea965ac8a880
Lior Sabag <lior@trendiguru.com>
2016-06-28 13:16:47 +0300

    tab clicked makes sense...
     if you think of tab as "a small flap or strip of material attached to or projecting from something"

--- app/main.js
+++ app/main.js
@@ -50,4 +50,4 @@ addEventListener('app opened', () => {
 
 // addEventListener('result clicked', e => analytics.track('Result Clicked', {clickUrl: e.info.clickURL, imageURL: window.app.props.imageURL}));
 
-addEventListener('tab set', e => analytics.track('Category Clicked', {title: e.info.title}));
+addEventListener('tab clicked', e => analytics.track('Category Clicked', {title: e.info.title}));

75123ce1a62d326ff0238b6b78a28512a1430b0e
Iddan Aharonson <mail@iddan.co>
2016-07-12 19:34:02 +0300

    Giphy loading

--- app/view/loading.js
+++ app/view/loading.js
@@ -1,13 +1,34 @@
+import {LOADING_GIPHIES} from 'constants';
+
 export default class Loading extends React.Component {
 
     constructor (props) {
         super(props);
     }
 
+    componentDidMount () {
+        this.setGiphy();
+    }
+
+    setGiphy () {
+        this.setState({giphy: LOADING_GIPHIES[getRandomInt(0, LOADING_GIPHIES.length - 1)]});
+    }
+
     render () {
-
-        return <div>LOADING</div>;
-
+        return <div>
+            <div className="giphy-loading" onClick={this.setGiphy.bind(this)} style={{
+                backgroundImage: `url('http://i.giphy.com/${this.state.giphy}.gif')`
+            }}></div>
+            LOADING
+        </div>;
     }
 
 }
+
+// from https://gist.github.com/kerimdzhanov/7529623
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//

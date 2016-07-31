89e41fa455ceb83d19172cfeeeb6e651389419a0
Iddan Aharonson <mail@iddan.co>
2016-07-31 18:40:49 +0300

    Beautiful, long arrow function

--- modules/selector.js
+++ modules/selector.js
@@ -1,7 +1,5 @@
 export default function Selector (whitelist, blacklist) {
-    return whitelist.map(white => {
-        return `${white},
-        ${white} > *${blacklist.map(black => `:not(${black})`).join('')},
-        ${white} *${blacklist.map(black => `:not(${black})`).join('')} *`;
-    }).join(', ');
+    return whitelist.map(white => `${white},
+${white} > *${blacklist.map(black => `:not(${black})`).join('')},
+${white} *${blacklist.map(black => `:not(${black})`).join('')} *`).join(', ');
 }

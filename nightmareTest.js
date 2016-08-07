f60f9dfe724523a39f332a5017388616175fedec
Iddan Aharonson <mail@iddan.co>
2016-08-07 14:19:08 +0300

    ES6

--- nightmareTest.js
+++ nightmareTest.js
@@ -1,15 +1,17 @@
-const WAIT_TIME = 10000;
+const Nightmare = require('nightmare');
 
-let RELEVANT_STACKS = ['smartCheckRelevancy', 'content'];
-let fzzPages = ['http://www.gala.de/stars/news/michelle-hunziker-die-schoensten-fotos-ihrer-familie_1166388-i10704220.html',
+const WAIT_TIME = 10000;
+const RELEVANT_STACKS = ['smartCheckRelevancy', 'content'];
+
+let fzzPages = [
+    'http://www.gala.de/stars/news/michelle-hunziker-die-schoensten-fotos-ihrer-familie_1166388-i10704220.html',
     'http://www.gala.de/beauty-fashion/fashion/fashion-looks-der-style-von-jennifer-lawrence_1171061_782392-i9736316.html',
     //'http://amaze-magazine.com/2016/07/fall-street-style-dress-pant-dressing/',
     'http://www.stylebook.de/stars/Lena-Meyer-Landrut-ueberrascht-mit-neuer-Frisur_1-784989.html'
 ];
 
-var Nightmare = require('nightmare');
-var checkPage = (url) => {
-    var nightmare = new Nightmare({
+function checkPage (url) {
+    let nightmare = new Nightmare({
         //show: true,
         openDevTools: true,
         switches: {
@@ -37,26 +39,23 @@ var checkPage = (url) => {
 }
 
 function checkStacks(pageName, stacks) {
-    var result = true;
-    RELEVANT_STACKS.forEach((name) => {
-        console.log(name + " stack contains: " + stacks[name].length + " elements");
+    let result = true;
+    for (let name of RELEVANT_STACKS) {
+        console.log(`${name} stack contains: ${stacks[name].length} elements`);
         result = stacks[name].length !== 0 && result;
-    });
-    console.log("____" + pageName + " check succeeded: " + result + "____");
+    }
+    console.log(`____${pageName} check succeeded: ${result}____`);
     return result;
 }
 
-var promises = [];
+let promises = fzzPages.map((page) => Promise.resolve(checkPage(page, checkStacks)));
 
-fzzPages.map(function(page) {
-    promises.push(Promise.resolve(checkPage(page, checkStacks)));
-});
-
-Promise.all(promises).then(function(results) {
-    var flag = true;
-    results.forEach(function(result) {
+Promise.all(promises)
+.then((results) => {
+    let flag = true;
+    for (let result of results) {
         flag = (flag && result);
-    })
-    console.log("_________TOTAL_RESULT_________: "+flag);
-    return flag
+    }
+    console.log(`_________TOTAL_RESULT_________${flag}`);
+    return flag;
 });

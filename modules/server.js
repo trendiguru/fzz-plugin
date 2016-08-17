397cf7131b44d877d7398e8fd6f20e3b2a64b235
Iddan Ahahronson <mail@iddan.co>
2016-08-17 18:24:31 +0300

    Deleted unused variable

--- modules/server.js
+++ modules/server.js
@@ -1,11 +1,11 @@
 /* eslint-disable console */
 
-import {API_URL, PID} from 'constants';
+import {API_URL} from 'constants';
 import {STACKS} from 'modules/devTools';
 import URLStore from './urlstore';
 import {Query} from './utils';
 
-let urlStore = new URLStore;
+let urlStore = new URLStore();
 
 export let smartCheckRelevancy = urlStore.append.bind(urlStore);
 

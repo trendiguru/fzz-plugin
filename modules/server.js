c633dbed9e9dbef45bbd5e7913cb2fb237983148
strodoff <starodub1995@ukr.net>
2016-08-17 11:37:38 +0300

    url is correct => we obtain results

--- modules/server.js
+++ modules/server.js
@@ -11,8 +11,7 @@ export let smartCheckRelevancy = urlStore.append.bind(urlStore);
 
 export function getImageData(imageUrl) {
     return fetch(API_URL + '&' + Query.stringify({
-        imageUrl,
-        PID
+        imageUrl
     }))
     .then(res => res.json());
 }

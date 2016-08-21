325f784b0f165682ecb74db472aada33ffb03224
strodoff <starodub1995@ukr.net>
2016-08-21 18:35:29 +0300

    added PID attribute to getImageData

--- modules/server.js
+++ modules/server.js
@@ -1,16 +1,16 @@
 /* eslint-disable console */
 
-import {API_URL} from 'constants';
+import {API_URL, PID} from 'constants';
 import {STACKS} from 'modules/devTools';
 import URLStore from './urlstore';
 import {Query} from './utils';
 
-let urlStore = new URLStore();
+let urlStore = new URLStore;
 
 export let smartCheckRelevancy = urlStore.append.bind(urlStore);
 
 export function getImageData(imageUrl) {
-    return fetch(API_URL +'&pid='+PID+'&'+ Query.stringify({
+    return fetch(API_URL + '&pid='+PID+'&' + Query.stringify({
         imageUrl
     }))
     .then(res => res.json());

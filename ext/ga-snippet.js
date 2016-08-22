a55ef14020b3e86c7030727fe68e2c74588556f1
Iddan Aharonson <mail@iddan.co>
2016-08-23 01:59:39 +0300

    Google Analytics Upgrade

--- ext/ga-snippet.js
+++ ext/ga-snippet.js
@@ -1,13 +1,12 @@
-export default function() {
-    (function(i,s,o,g,r,a,m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments);
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        (m ? m.parentNode : document.documentElement).insertBefore(a,m);
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-}
+(function(i,s,o,g,r,a,m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments);
+    },
+    i[r].l = 1 * new Date();
+    a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    (m ? m.parentNode : document.documentElement).insertBefore(a,m);
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');

86a335aefb5f32a6d02a9af1cd97f8fbfbff6bd7
strodoff <starodub1995@ukr.net>
2016-08-30 22:32:21 +0300

    deleted deleted test functions

--- extensions/chrome_dev/assets/js/popup.js
+++ extensions/chrome_dev/assets/js/popup.js
@@ -27,26 +27,8 @@ let props = {
         position: 'relative',
     }
 };
-//-------------------test-------------------------------------------------
-// chrome.tabs.query({
-//     active: true,
-//     currentWindow: true
-// }, function(tabs) {
-//     chrome.tabs.sendMessage(tabs[0].id, {postKey: "hello"}, function(response) {
-//         console.log(response.farewell);
-//     });
-// });
-//
-// chrome.tabs.query({
-//     active: true,
-//     currentWindow: true
-// }, function(tabs) {
-//     chrome.tabs.sendMessage(tabs[0].id, {postKey: "devTools"}, function(response) {
-//         console.log(response);
-//     });
-// });
+
 getAnswer("devTools").then((answer)=>{console.log(answer);});
-//------------------------end_test---------------------------------------
 
 domready(() => {
     ReactDOM.render(createBoard(15).render(), createWrapper());

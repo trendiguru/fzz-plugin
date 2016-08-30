5b86a96468f576f882f7aa6065a098275da3ef90
strodoff <starodub1995@ukr.net>
2016-08-30 12:10:02 +0300

    we didn't run serve on start

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ import {Query} from 'modules/utils';
 
 let {blacklist, whitelist, pid, api} = new FzzDataAttributes();
 let extUrl = extension ? extension.getURL('').substr(0, extension.getURL('').length - 1) : null;
-
+console.log(extUrl);
 export const HOST_DOMAIN = {
         DEV: extUrl,
         PRODUCTION: 'https://fzz.storage.googleapis.com',

1117260bca2ed4a4c059fc189c20e090e9960dc2
Iddan Aharonson <mail@iddan.co>
2016-09-18 14:51:22 +0300

    CSS Smooth transitions
    using transform

--- app/view/tab.js
+++ app/view/tab.js
@@ -3,6 +3,13 @@ export class TabView extends React.Component {
         super(props);
         this.state = {tab: 0};
     }
+    static get propTypes () {
+        return {
+            children: React.PropTypes.array.isRequired,
+            aside: React.PropTypes.array,
+            select: React.PropTypes.number,
+        };
+    }
     setTab (i) {
         this.setState({tab: i});
         let e = Object.assign(new Event('tab set', {bubbles: true}), {
@@ -16,20 +23,24 @@ export class TabView extends React.Component {
         this.setState({tab: 0});
     }
     render () {
-        let {children} = this.props,
-            TitleNodes = [],
-            TabNodes = [];
-        children = Array.isArray(children) ? children : [children];
+        let children = toArray(this.props.children);
+        let TitleNodes = [];
+        let TabNodes = [];
+        let liWidth = 100 / children.length || 0;
         children.forEach((tab, i) => {
             TitleNodes.push(<li key={i} onClick={this.setTab.bind(this, i)} className={this.state.tab == i ? 'select' : ''}>{tab.props.title}</li>);
             TabNodes.push(React.cloneElement(tab, {key: i, index: i, select: this.state.tab}));
         });
-        let liWidth = 100 / children.length || 0;
+        console.log(`translateX(${this.state.tab * 100}%)`);
         return <div ref="root">
             <nav>
                 <ul>
                     {TitleNodes}
-                    <label className="indicator" style={{left: this.state.tab * liWidth + '%', width: liWidth + '%'}}></label>
+                    <label className="indicator" style={{
+                        width: liWidth + '%',
+                        transform: `translateX(${this.state.tab * 100}%)`,
+                        webkitTransform: `translateX(${this.state.tab * 100}%)`
+                    }}></label>
                 </ul>
                 <aside>{this.props.aside}</aside>
             </nav>
@@ -38,11 +49,20 @@ export class TabView extends React.Component {
     }
 }
 
-export class Tab extends React.Component {
-    constructor (props) {
-        super(props);
-    }
-    render () {
-        return <section style={{width: '100%', left: (this.props.index - this.props.select) * 100 + '%', top: 0, position: 'absolute'}}>{this.props.children}</section>;
-    }
+export function Tab (props) {
+    return <section style={{
+        width: '100%',
+        transform: `translateX(${(props.index - props.select) * 100}%)`,
+        webkitTransform: `translateX(${(props.index - props.select) * 100}%)`,
+        top: 0,
+        position: 'absolute'
+    }}>{props.children}</section>;
 }
+
+Tab.propTypes = {
+    index: React.PropTypes.number,
+    select: React.PropTypes.number,
+    children: React.PropTypes.element
+};
+
+let toArray = (array) => Array.isArray(array) ? array : [array];

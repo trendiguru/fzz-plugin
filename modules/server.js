4b36f51cf2beadac7294ffc90a025a1cd1f2ac3c
strodoff <starodub1995@ukr.net>
2016-10-06 14:41:33 +0300

    hotfix-1.1.3 fetch 500 problem (endless loading of iframe animation)

--- modules/server.js
+++ modules/server.js
@@ -10,6 +10,15 @@ export let urlStore = new URLStore;
 export let smartCheckRelevancy = urlStore.append.bind(urlStore);
 
 export function getImageData(imageUrl) {
-    return fetch(API_URL + '?' + Query.stringify({imageUrl, pid: PID}))
-    .then(res => res.json());
+    return fetch(API_URL + '?' + Query.stringify({imageUrl, pid: PID})).then((res) =>{
+        try{
+            if (res.status===500){
+                throw {name: '500!'};
+            }
+            return res.json();
+        }catch(e){
+            console.error(e);
+            return null;
+        }
+    });
 }

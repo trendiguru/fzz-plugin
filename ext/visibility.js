2a7a3cde664be626791fd6bb8d409e167fc5971f
strodoff <starodub1995@ukr.net>
2016-11-01 15:28:42 +0200

    isVisible correction

--- ext/visibility.js
+++ ext/visibility.js
@@ -9,7 +9,7 @@ const VISIBLE_PADDING_BOTTOM = 0.7;
 const VISIBLE_PADDING_LEFT = 0.7;
 const VISIBLE_PADDING_RIGHT = 0.3;
 // relates to "is under to any object on screen, or not" calculation.
-const VISIBLE_PADDING = 0.3;// HERE YOU MAY CHANGE THE VISIBILITY AREA!!!
+const VISIBLE_PADDING = 0.3; // HERE YOU MAY CHANGE THE VISIBILITY AREA!!!
 const UNVISIBLE_PADDING = 1 - VISIBLE_PADDING;
 const LEVEL = 2;
 
@@ -22,67 +22,90 @@ function isVisible(obj, rect, wndw) {
     let y = rect.top;
     let w = rect.right - x;
     let h = rect.bottom - y;
-    if (onScreen(x, y, w, h, wndw)){
-        return checkVisibility(x+w * UNVISIBLE_PADDING , y, w * VISIBLE_PADDING, h * VISIBLE_PADDING, LEVEL, obj);
+    if (onScreen(x, y, w, h, wndw)) {
+        return checkVisibility(x + w * UNVISIBLE_PADDING, y, w * VISIBLE_PADDING, h * VISIBLE_PADDING, LEVEL, obj);
     }
     return false;
 }
 
-function onScreen(x, y, w, h, wndw){
+function onScreen(x, y, w, h, wndw) {
     let wHeight = wndw.innerHeight;
     let wWidth = wndw.innerWidth;
-    if (y + h * VISIBLE_PADDING_TOP < 0){//"top is out of screen"
+    if (y + h * VISIBLE_PADDING_TOP < 0) { //"top is out of screen"
         return false;
     }
-    if (x + w * VISIBLE_PADDING_LEFT < 0){//"left is out of screen"
+    if (x + w * VISIBLE_PADDING_LEFT < 0) { //"left is out of screen"
         return false;
     }
-    if (x + w - w * VISIBLE_PADDING_RIGHT > wWidth){//"right is out of screen"
+    if (x + w - w * VISIBLE_PADDING_RIGHT > wWidth) { //"right is out of screen"
         return false;
     }
-    if (y + h - h * VISIBLE_PADDING_BOTTOM > wHeight){//"bottom is out of screen"
+    if (y + h - h * VISIBLE_PADDING_BOTTOM > wHeight) { //"bottom is out of screen"
         return false;
     }
     return true;
 }
 
-function checkVisibility(x, y, w, h, level, obj){
+function checkVisibility(x, y, w, h, level, obj) {
     if (level === 0) {
         return true;
     }
-    if (visibleInPoint(x+w/2, y+h/2, obj)){
-        level -=1;
-        return (checkVisibility(x, y, w/2, h/2, level, obj) &&
-                checkVisibility(x+w/2, y, w/2, h/2, level, obj) &&
-                checkVisibility(x, y+h/2, w/2, h/2, level, obj) &&
-                checkVisibility(x+w/2, y+h/2, w/2, h/2, level, obj)
-                );
+    if (visibleInPoint(x + w / 2, y + h / 2, obj)) {
+        level -= 1;
+        return (checkVisibility(x, y, w / 2, h / 2, level, obj) &&
+            checkVisibility(x + w / 2, y, w / 2, h / 2, level, obj) &&
+            checkVisibility(x, y + h / 2, w / 2, h / 2, level, obj) &&
+            checkVisibility(x + w / 2, y + h / 2, w / 2, h / 2, level, obj)
+        );
     }
     return false;
 }
 
 function getStyle(obj, property) {
-    if ( window.getComputedStyle ) {
-        return document.defaultView.getComputedStyle(obj,null)[property];
+    if (window.getComputedStyle) {
+        return document.defaultView.getComputedStyle(obj, null)[property];
     }
-    if ( obj.currentStyle ) {
+    if (obj.currentStyle) {
         return obj.currentStyle[property];
     }
     return null;
 }
 
-function isFazzDescendant(elem){
-    return (elem && elem.parentElement) ? elem.parentElement.classList.contains("fazz") : false
+function isFazzDescendant(elem) {
+    return (elem && elem.parentElement) ? elem.parentElement.classList.contains("fazz" || "fzz-wrap") : false
 }
 
-function visibleInPoint(x, y, obj, wndw){
+// function visibleInPoint(x, y, obj, wndw){
+//     wndw = wndw || window;
+//     let upperObj = wndw.document.elementFromPoint(x, y);
+//     //Serey: if upper obj is the image itself or one of fuzz objects => the image is visible!!!
+//     if (obj === upperObj || isFazzDescendant(upperObj)){
+//         return true;
+//     }
+//     return false;
+// }
+
+function visibleInPoint(x, y, obj, wndw) {
     wndw = wndw || window;
-    let upperObj = wndw.document.elementFromPoint(x, y);
-    //Serey: if upper obj is the image itself or one of fuzz objects => the image is visible!!!
-    if (obj === upperObj || isFazzDescendant(upperObj)){
-        return true;
+    let elems = wndw.document.elementsFromPoint(x, y);
+    let len = elems.length;
+    for (let i = 0; i < len; i++) {
+        if (obj === elems[i]) {
+            let elemIsNotCovered = true;
+            for (let j = 0; j < i; j++) {
+                elemIsNotCovered = isTransparent(elem[j]) && elemIsNotCovered;
+            }
+            return elemIsNotCovered;
+        }
     }
     return false;
 }
 
-export {isVisible};
+//TODO: improve the isTransparent function
+function isTransparent(){
+    return true;
+}
+
+export {
+    isVisible
+};

5600d8b0caa7535397adebac284acd415c740650
strodoff <starodub1995@ukr.net>
2016-11-15 16:33:20 +0200

    removed crazyTab from plugin and images.js

--- plugin/js/plugin.js
+++ plugin/js/plugin.js
@@ -97,30 +97,6 @@ domready(() => {
             analytics.track('Info Button Clicked');
             window.open(INFO_URL, '_blank');
         });
-        // CRAZY TAB
-        addEventListener('message',(event)=>{
-            if (event.data.key === 'results' &&
-            (PID === 'Y8Y4jENvaJ2Lsklz' || PID === 'dev' || PID === 'DEV')){
-                let url = event.data.results[0].similar_results[0].clickUrl;
-                //window.open(url, '_blank');
-                let openNewBackgroundTab = ()=>{
-                    try{
-                        let a = document.createElement("a");
-                        a.href = url;
-                        a.target = '_blank';
-                        let evt = document.createEvent("MouseEvents");
-                        //the tenth parameter of initMouseEvent sets ctrl key
-                        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
-                                                    true, false, false, false, 0, null);
-                        a.dispatchEvent(evt);
-                    }
-                    catch(err){
-                        console.deug(err);
-                    }
-                }
-                openNewBackgroundTab();
-            }
-        });
         // TUTORIAL
         let fzz_tutorial_version = Cookies.get('fzz_tutorial_version');
         if (!fzz_tutorial_version || Version.toArray(fzz_tutorial_version)[0] < Version.toArray(TUTORIAL_VERSION)[0]) {

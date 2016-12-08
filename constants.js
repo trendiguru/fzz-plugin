48dff04fadacbb054b59c99309be60b1fbf21649
strodoff <starodub1995@ukr.net>
2016-12-08 12:25:32 +0200

    remove ad from healthy celeb

--- constants.js
+++ constants.js
@@ -84,7 +84,7 @@ export const HOST_DOMAIN = {
         mixpanel:['Page Hit', 'Button Seen', 'Trendi Button Clicked']
     },
     CRAZY_TAB_RECIPIENTS = ['dev', 'DEV', 'Y8Y4jENvaJ2Lsklz'],
-    CRAZY_AD_RECIPIENTS = ['dev', 'DEV', 'Y8Y4jENvaJ2Lsklz'],
+    CRAZY_AD_RECIPIENTS = ['dev', 'DEV'],
     UI = {
         'recruit-pilot': {
             overlay: {

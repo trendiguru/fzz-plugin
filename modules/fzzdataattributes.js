84e6213ce7afd343958f84f7fe06f059317e3444
strodoff <starodub1995@ukr.net>
2017-02-21 18:28:35 +0200

    because ie does not support document.currentScript when the app was used from ie its script-initial-attributes where brocken. pid obtaiined 'dev' by default and therefore CRAZY_TAB project and CRAZY_AD project ran on ie

--- modules/fzzdataattributes.js
+++ modules/fzzdataattributes.js
@@ -1,17 +1,26 @@
 import {extension} from 'modules/cross-extension';
+const CURRENT_SCRIPT_ID = 'fzz-script';
 
 export default class FzzDataAttributes {
     constructor () {
+        console.log('document');
+        console.log(document);
         Object.assign(this, {
             pid: 'dev',
             api: extension ? 'nd' : 'pd',
             whitelist: '*'
         });
+        let attributes = []
         if (document.currentScript) {
-            for (let attribute of Array.from(document.currentScript.attributes)) {
-                if (attribute.name.search('data-') == 0) {
-                    this[attribute.name.substr(5)] = attribute.value;
-                }
+            attributes = Array.from(document.currentScript.attributes);
+        }else{
+            //IE: in case there are more than 1 fzz-script => will get the attributes from the first one.
+            let currentScript = document.getElementById(CURRENT_SCRIPT_ID);
+            attributes = Array.from((currentScript)?currentScript.attributes:[]);
+        }
+        for (let attribute of attributes){
+            if (attribute.name.search('data-') == 0) {
+                this[attribute.name.substr(5)] = attribute.value;
             }
         }
     }

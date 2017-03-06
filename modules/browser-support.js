928e70d26719abd5c3707fc7590da5c0659a5d39
strodoff <starodub1995@ukr.net>
2017-03-06 14:57:09 +0200

    changed the structure of BROWSER_SUPPORT object, and functions of browser-support.js. included chrome 56+, and safari+ for mobile for stylebook.

--- modules/browser-support.js
+++ modules/browser-support.js
@@ -1,8 +1,8 @@
 import bowser from 'bowser';
 import {PID} from 'constants';
 
-const DEFAULT_MIN_VERSION = '-1';
-const DEFAULT_MAX_VERSION = '1000';
+const DEFAULT_MIN_VERSION = '1';
+const DEFAULT_MAX_VERSION = '100';
 const ua = window.navigator.userAgent;
 const BROWSER_SUPPORT = {
     //stylebook:
@@ -47,8 +47,36 @@ const BROWSER_SUPPORT = {
                 EXCLUDE:[],
             },
         },
-        tablet: false,
-        mobile: false,
+        tablet: true,
+        mobile: true,
+        mobileBrowsers:{
+            chrome:{
+                MIN_VERSION:'56',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+            safari:{
+                MIN_VERSION:'9',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+        },
+        tabletBrowsers:{
+            chrome:{
+                MIN_VERSION:'56',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+            safari:{
+                MIN_VERSION:'9',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+        }
     },
         'dev':{
         browsers:{
@@ -99,18 +127,47 @@ const BROWSER_SUPPORT = {
         },
         tablet: true,
         mobile: true,
+        mobileBrowsers:{
+            chrome:{
+                MIN_VERSION:'56',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+            safari:{
+                MIN_VERSION:'9',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+        },
+        tabletBrowsers:{
+            chrome:{
+                MIN_VERSION:'56',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+            safari:{
+                MIN_VERSION:'9',
+                MAX_VERSION:DEFAULT_MAX_VERSION,
+                INCLUDE:[],
+                EXCLUDE:[],
+            },
+        }
     }
 };
 
-function _versionControl(browserName){
-    if (BROWSER_SUPPORT[PID]['browsers'][browserName].EXCLUDE.indexOf(bowser.version)!==-1){
+function _versionControl(browserName, browsersListName){
+    if (BROWSER_SUPPORT[PID][browsersListName][browserName].EXCLUDE.indexOf(bowser.version)!==-1){
         return false;
     }
-    if (BROWSER_SUPPORT[PID]['browsers'][browserName].INCLUDE.indexOf(bowser.version)!==-1){
+    if (BROWSER_SUPPORT[PID][browsersListName][browserName].INCLUDE.indexOf(bowser.version)!==-1){
         return true;
     }
-    let minVersion = Number(BROWSER_SUPPORT[PID]['browsers'][browserName].MIN_VERSION.split('.')[0]);
-    let maxVersion = Number(BROWSER_SUPPORT[PID]['browsers'][browserName].MAX_VERSION.split('.')[0]);
+    let minVersion = BROWSER_SUPPORT[PID][browsersListName][browserName].MIN_VERSION;
+    let maxVersion = BROWSER_SUPPORT[PID][browsersListName][browserName].MAX_VERSION;
+    let currentVersion = bowser.version;
     //compareVersions returns -1, 1 or 0.
     return (bowser.compareVersions([maxVersion, currentVersion])>=0 && bowser.compareVersions([currentVersion, minVersion])>=0);
 }
@@ -118,10 +175,9 @@ function _versionControl(browserName){
 
 export default function(){
     let browserSupportStatus = false;
-    let browserNames = (BROWSER_SUPPORT[PID])? Object.keys(BROWSER_SUPPORT[PID]['browsers']):undefined;
     // if does not exist the browsers-support-definition for current PID => run the script independently.
     //TODO: create a default browsers-support-definition.
-    if (!browserNames){
+    if (!BROWSER_SUPPORT[PID]){
         return true;
     }
     //check if supports mobile-browsers:
@@ -132,9 +188,13 @@ export default function(){
     if (!BROWSER_SUPPORT[PID].tablet && bowser.tablet){
         return false;
     }
+    let browsersByDevise = (bowser.mobile? 'mobileBrowsers': null) || 
+                           (bowser.tablet? 'tabletBrowsers': null) || 
+                           'browsers';
+    let browserNames = (BROWSER_SUPPORT[PID][browsersByDevise])? Object.keys(BROWSER_SUPPORT[PID][browsersByDevise]):[];
     for(let key of browserNames){
         if (bowser[key]){
-            browserSupportStatus=_versionControl(key);
+            browserSupportStatus=_versionControl(key, browsersByDevise);
         }
     }
     if (browserSupportStatus===false){

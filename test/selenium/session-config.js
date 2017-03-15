ade555c08d91a95ef90473969761924f7582d5e1
strodoff <starodub1995@ukr.net>
2017-03-15 13:43:52 +0200

    added firefox 'capability' in session-config.js

--- test/selenium/session-config.js
+++ test/selenium/session-config.js
@@ -1,5 +1,8 @@
+const USER = 'admin@trendiguru.com';
+const PASSWORD = 'u1c12cb9333d4b94';
 const CONFIG=[
     {
+        //ie11 Win7x64
         remoteHub: 'http://hub.crossbrowsertesting.com:80/wd/hub', 
         caps: {
             name : 'Basic Example',
@@ -10,8 +13,24 @@ const CONFIG=[
             record_video : 'true',
             record_network : 'true',
             browserName : 'internet explorer', //firefox, internet explorer, chrome, opera, or safari
-            username : 'admin@trendiguru.com',
-            password : 'u1c12cb9333d4b94',
+            username : USER,
+            password : PASSWORD,
+        },
+    },
+    {
+        //firefox46 Win7x64
+        remoteHub: 'http://hub.crossbrowsertesting.com:80/wd/hub', 
+        caps: {
+            name : 'Basic Example',
+            build :  '1.0',
+            browser_api_name : 'FF46x64', 
+            os_api_name : 'Win7x64-C1', 
+            screen_resolution : '1366x768',
+            record_video : 'true',
+            record_network : 'true',
+            browserName : 'firefox', //firefox, internet explorer, chrome, opera, or safari
+            username : USER,
+            password : PASSWORD,
         },
     },
 ];
